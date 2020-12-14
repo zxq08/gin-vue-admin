f7eeb29ee7247d587fe9d27e1476ce569cc3624e
pixel <303176530@qq.com>
2020-12-14 18:16:29 +0800

    断点续传手工原理展示版本VUE修改完成

--- web/src/view/layout/aside/asideComponent/index.vue
+++ web/src/view/layout/aside/asideComponent/index.vue
@@ -1,39 +1,48 @@
 <template>
   <component :is="menuComponent" :routerInfo="routerInfo" v-if="!routerInfo.hidden">
-    <template v-if="routerInfo.children&&routerInfo.children.length">
-      <AsideComponent :key="item.name" :routerInfo="item" v-for="item in routerInfo.children" />
+    <template v-if="routerInfo.children && routerInfo.children.length">
+      <AsideComponent
+        :key="item.name"
+        :routerInfo="item"
+        v-for="item in routerInfo.children"
+      />
     </template>
   </component>
 </template>
 
 <script>
-import MenuItem from './menuItem'
-import AsyncSubmenu from './asyncSubmenu'
+import MenuItem from "./menuItem";
+import AsyncSubmenu from "./asyncSubmenu";
+import { computed } from "vue";
 
 export default {
-  name: 'AsideComponent',
-  computed: {
-    menuComponent() {
-      if (this.routerInfo.children&&this.routerInfo.children.filter(item=>!item.hidden).length) {
-        return 'AsyncSubmenu'
-      } else {
-        return 'MenuItem'
-      }
-    }
-  },
+  name: "AsideComponent",
   props: {
     routerInfo: {
-      default: function() {
-        return null
+      default: function () {
+        return null;
       },
-      type: Object
-    }
+      type: Object,
+    },
   },
   components: {
     MenuItem,
-    AsyncSubmenu
-  }
-}
+    AsyncSubmenu,
+  },
+  setup(props) {
+    const menuComponent = computed(() => {
+      if (
+        props.routerInfo.children &&
+        props.routerInfo.children.filter((item) => !item.hidden).length
+      ) {
+        return "AsyncSubmenu";
+      } else {
+        return "MenuItem";
+      }
+    });
+
+    return{menuComponent }
+  },
+};
 </script>
-<style lang="scss">
-</style>
+<style lang="scss"></style>
